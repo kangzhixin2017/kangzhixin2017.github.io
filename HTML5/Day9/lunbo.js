ccca285dc4678bd0543b432c2fd9ee7deedb41db
kangsir <315206916@qq.com>
2018-07-25 10:59:36 +0800

    html 学习demo

---
+++ HTML5/Day9/lunbo.js
@@ -0,0 +1,14 @@
+$(".lunbo").mousemove(function(e){
+	console.log(e.pageX);
+	$(".lunbo").css("margin-left":"")
+})
+//$(".lunbo").click(function(){
+//	alert("22");
+////})
+//document.getElementById("lunbo").addEventListener("touchmove",function(e){
+//	var a=e.touches[0].pageX;
+//	console.log(a);
+//})
+//$(document).on("swiperight",".lunbo",function(){
+//	alert("11");
+//})
