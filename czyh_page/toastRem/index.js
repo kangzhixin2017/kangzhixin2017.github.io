72900778bd1049578a5e06a7329e267be7854799
kangsir <315206916@qq.com>
2020-05-20 15:52:26 +0800

    1

---
+++ czyh_page/toastRem/index.js
@@ -0,0 +1,1 @@
+document.documentElement.clientWidth > 375 ? document.documentElement.style.fontSize = 375 / 375 + 'px' : document.documentElement.style.fontSize = document.documentElement.clientWidth / 375 + 'px';
