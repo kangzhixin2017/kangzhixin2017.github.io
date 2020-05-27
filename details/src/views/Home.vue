035ed635605f46ef0f2c3f0fbe5fa2135335292f
kangsir <315206916@qq.com>
2020-05-27 15:33:21 +0800

    1

--- details/src/views/Home.vue
+++ details/src/views/Home.vue
@@ -145,10 +145,8 @@ export default {
         for (let i = 0; i < 10; i++) {
           this.list.push(this.list.length + 1);
         }
-
         // 加载状态结束
         this.loading = false;
-
         // 数据全部加载完成
         if (this.list.length >= 40) {
           this.finished = true;
