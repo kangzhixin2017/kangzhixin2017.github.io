3f3d47ec7173ec909a56cc0691ad1de2fb85b14c
kangsir <315206916@qq.com>
2020-06-11 22:49:42 +0800

    1

--- czyh_orange/js/index.js
+++ czyh_orange/js/index.js
@@ -9,8 +9,8 @@
 //	document.documentElement.clientHeight;
 //	document.getElementsByTagName("html")[0].style.fontSize = '6' + "px"
 //};
-
-document.documentElement.clientWidth > 375 ? document.documentElement.style.fontSize = 375 / 37.5 + 'px' : document.documentElement.style.fontSize = document.documentElement.clientWidth / 37.5 + 'px';
+document.documentElement.style.fontSize = '1px'
+//document.documentElement.clientWidth > 375 ? document.documentElement.style.fontSize = 375 / 375 + 'px' : document.documentElement.style.fontSize = document.documentElement.clientWidth / 37.5 + 'px';
 //alert(document.documentElement.style.fontSize)
 /**
  * tab
