604f0d1f8518c04213a066e9cbc34301aba413d8
kangsir <315206916@qq.com>
2020-07-09 10:00:22 +0800

    1

--- czyh_page/game/js/prize.js
+++ czyh_page/game/js/prize.js
@@ -1,47 +1,76 @@
 //alert('s')
 
+var nowIndex = 1;
+var pageSize = 15;
+var isRequest = true;
 var Data;
-http(URL.prizeList, {
-	token: window.localStorage.getItem('token')
-}).then(e => {
-	if(!e.length) {
-		$('.nolist').show()
-	} else {
-		Data = e;
-		for(var i = 0; i < e.length; i++) {
-			var img = window.localStorage.getItem('URL_HEAD') + e[i].img;
-			var time = formatDate(e[i].create_time)
-			if(e[i].order_status == 4) {
-				$('.list').append(`<div class="item">
+initData()
+
+function initData() {
+	http(URL.prizeList, {
+		token: window.localStorage.getItem('token'),
+		pageNum: nowIndex,
+		pageSize: pageSize
+	}).then(e => {
+		if(!e.total) {
+			$('.nolist').show()
+		} else {
+			Data = e.list;
+			if(Data.length == 0){
+				$('.dataNO').show()
+				return;
+			}
+			for(var i = 0; i < Data.length; i++) {
+				var img = window.localStorage.getItem('URL_HEAD') + Data[i].img;
+				var time = formatDate(Data[i].create_time)
+				if(Data[i].order_status == 4) {
+					$('.list').append(`<div class="item">
 					<div class="left">
 						<img src="${img}" />
 						<div>
-							<div class="top">${e[i].name}</div>
+							<div class="top">${Data[i].name}</div>
 							<div class="bottom">${time}</div>
 						</div>
 					</div>
 					<div class="right" onclick="toinfo(${i})">去领取</div>
 				</div>`)
-			} else {
-				$('.list').append(`<div class="item">
+				} else {
+					$('.list').append(`<div class="item">
 					<div class="left">
 						<img src="${img}" />
 						<div>
-							<div class="top">${e[i].name}</div>
+							<div class="top">${Data[i].name}</div>
 							<div class="bottom">${time}</div>
 						</div>
 					</div>
 					<div class="right2">已领取</div>
 				</div>`);
+				}
 			}
 		}
-	}
-})
+		isRequest = true;
+	})
+}
 
 function toinfo(i) {
-	console.log('点击的奖品：',Data[i])
+	console.log('点击的奖品：', Data[i])
 	window.localStorage.setItem('prize_img', window.localStorage.getItem('URL_HEAD') + Data[i].img)
 	window.localStorage.setItem('prize_name', Data[i].name)
 	window.localStorage.setItem('orderNo', Data[i].order_no)
 	window.location.href = 'information.html'
 }
+
+$(window).scroll(function(event) {
+	var wScrollY = window.scrollY; // 当前滚动条位置
+	var wInnerH = window.innerHeight; // 设备窗口的高度（不会变）
+	var bScrollH = document.body.scrollHeight; // 滚动条总高度
+//	console.log(wScrollY, wInnerH, bScrollH)
+	if(wScrollY + wInnerH >= bScrollH) {
+		if(isRequest) {
+			isRequest = false;
+			console.log('到底了')
+			nowIndex++;
+			initData()
+		}
+	}
+})
